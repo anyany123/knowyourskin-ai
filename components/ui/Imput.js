9cb9bb8ec4550fa4399ee8988fd7fc063cee5d5e
anyany123 <pitp9202@gmail.com>
2025-04-08 13:23:39 +0530

    Create Imput.js

---
+++ components/ui/Imput.js
@@ -0,0 +1,12 @@
+// components/ui/input.js
+
+import React from "react";
+
+export function Input({ className, ...props }) {
+  return (
+    <input
+      className={`border border-gray-300 rounded-lg px-3 py-2 ${className}`}
+      {...props}
+    />
+  );
+}
