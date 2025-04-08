a531cdf45d7b7a606af76422a8d5b434392d377d
anyany123 <pitp9202@gmail.com>
2025-04-08 13:23:05 +0530

    Create Button.js

---
+++ components/ui/Button.js
@@ -0,0 +1,14 @@
+// components/ui/button.js
+
+import React from "react";
+
+export function Button({ children, className, ...props }) {
+  return (
+    <button
+      className={`px-4 py-2 rounded-lg font-semibold ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
