70de120f065ec590f24130e7a267c1d6e2a77a66
anyany123 <pitp9202@gmail.com>
2025-04-08 14:01:28 +0530

    Update card.js

--- components/ui/card.js
+++ components/ui/card.js
@@ -1,1 +1,15 @@
+export function Card({ children, className = "" }) {
+  return (
+    <div className={`bg-white border rounded-lg shadow-md ${className}`}>
+      {children}
+    </div>
+  );
+}
 
+export function CardContent({ children, className = "" }) {
+  return (
+    <div className={`p-4 ${className}`}>
+      {children}
+    </div>
+  );
+}
